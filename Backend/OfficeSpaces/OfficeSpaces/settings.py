683675443f7f0196e04fbbf2ae68b089aa3ebbbc
DeepNanda <deepnanda30@gmail.com>
2020-07-11 23:39:09 +0530

    Model Added

--- Backend/OfficeSpaces/OfficeSpaces/settings.py
+++ Backend/OfficeSpaces/OfficeSpaces/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- Backend/OfficeSpaces/OfficeSpacesRest/models.py
+++ Backend/OfficeSpaces/OfficeSpacesRest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    photo=models.ImageField()
+    address=models.CharField(max_length=256)
+    user_ref = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Social_disancing_violations:
+    number_of_violations=models.IntegerField()
+    timestamp=models.DateTimeField()
+    photo_violation=models.ImageField()
