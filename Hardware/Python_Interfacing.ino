d126bd7ada202dffc735a91967e0da68939b0161
Jash271 <51506649+Jash271@users.noreply.github.com>
2020-08-26 12:00:44 +0530

    Temperature Sensor Integrated 🌡

--- Hardware/Python_Interfacing.ino
+++ Hardware/Python_Interfacing.ino
@@ -1,5 +1,7 @@
 int data;
 int LED=13;
+float temp;
+int tempPin = 0;
 
 void setup() { 
   Serial.begin(9600);                               //initialize serial COM at 9600 baudrate
@@ -10,17 +12,21 @@ void setup() {
 }
  
 void loop() {
+temp = analogRead(tempPin);
+temp = temp * 0.48828125;
+
 printf("%s",Serial.available());
 while (Serial.available())    //whatever the data that is coming in serially and assigning the value to the variable “data”
 
 { 
 printf("%d",1);
 data = Serial.read();
-printf("%s",data);
+printf("%s",data);q
 
 
 
-if (data == '1'){
+if (data == '1' && temp < 35.0){
+
 
 digitalWrite (LED, HIGH); 
 }
