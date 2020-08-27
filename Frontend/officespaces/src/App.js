2fb91c42c8d1d6e2850c7c1644108b57896fc684
Jenish Hirpara <jenishhirpara23@gmail.com>
2020-08-27 18:22:27 +0530

    added employee-detail page

--- Frontend/officespaces/src/App.js
+++ Frontend/officespaces/src/App.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SignIn from './Components/SignIn'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Dashboard from './Components/Dashboard'
-import Attendance from './Components/Attendance'
-import Announcements from './Components/Announcements'
-import Navbar from './Components/Navbar'
-import Form from './Components/Form'
-import { Provider } from 'react-redux';
-import store from './store';
-import 'materialize-css/dist/css/materialize.min.css';
-import CartBtns from './Components/CartBtns';
+import React, { useEffect } from "react";
+import "./App.css";
+import SignIn from "./Components/SignIn";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Attendance from "./Components/Attendance";
+import EmployeeDetail from "./Components/EmployeeDetail";
+import Announcements from "./Components/Announcements";
+import Navbar from "./Components/Navbar";
+import Form from "./Components/Form";
+import { Provider } from "react-redux";
+import store from "./store";
+import "materialize-css/dist/css/materialize.min.css";
+import CartBtns from "./Components/CartBtns";
 // import M from 'materialize-css/dist/js/materialize.min.js';
-
 
 function App() {
   // useEffect(() => {
@@ -26,11 +25,12 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route exact path='/' exact component={SignIn} />
-          <Route exact path='/dashboard' exact component={Dashboard} />
-          <Route exact path='/attendance' exact component={Attendance} />
-          <Route exact path='/announcement' exact component={Announcements} />
-          <Route exact path='/add_announcement' exact component={Form} />
+          <Route exact path="/" exact component={SignIn} />
+          <Route exact path="/dashboard" exact component={Dashboard} />
+          <Route exact path="/attendance" exact component={Attendance} />
+          <Route exact path="/announcement" exact component={Announcements} />
+          <Route exact path="/add_announcement" exact component={Form} />
+          <Route exact path="/employee_detail" component={EmployeeDetail} />
         </Switch>
         <CartBtns />
       </BrowserRouter>
